52e188e5531f9c85dcdc72cc269017ad3f5a8662
Adeshina Ibrahim <debowale01@gmail.com>
2021-09-13 15:41:44 +0100

    [Listing Schema]: Closes #11, added the lisitng schema

---
+++ backend/models/listingModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+const requiredString = {
+  type: String,
+  trim: true,
+}
+const listingSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'A listing must be created by a user']
+  },
+  title: {
+    ...requiredString,
+    required: [true, 'A lisitng must have a title']
+  }, 
+  slug: String,
+  description: {
+    ...requiredString,
+    required: [true, 'A listing must have a description']
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  expiryData: {
+    type: Date,
+  },
+  tags:  [String]
+}, {
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
+})
+
+const Lisitng = mongoose.model('Listing', listingSchema)
+
+export default Lisitng
