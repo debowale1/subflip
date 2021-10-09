257ef80bf714dc257da70c206ad6bf629d1ac0b6
Adeshina Ibrahim <debowale01@gmail.com>
2021-10-09 12:19:43 +0100

    rate limiting for our api

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan'
 import userRouter from './routes/userRoutes.js'
 import authRouter from './routes/authRoutes.js'
 import listingRouter from './routes/listingRoutes.js'
+import rateLimit from 'express-rate-limit'
 
 const app = express()
 dotenv.config()
@@ -15,6 +16,14 @@ if (process.env.NODE_ENV === 'development') {
 
 // body parser
 app.use(express.json())
+
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60*60*1000,
+	message: 'Too many requests from this IP. Please try again in an hour'
+})
+
+app.use('/api', limiter)
 
 app.get('/api/v1/ping', (req, res) => {
 	res.status(200).json({
